1bbacb50cff2c776b71d4245cd95de5e83faf9ce
saiabhiteja <saiabhiteja29@gmail.com>
2025-07-06 10:56:51 +0530

    Update config with correct GitHub username for deployment

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,12 +2,12 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'saiabhiteja', // Your GitHub org/user name. (This is the only required config)
   },
   /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/saiabhiteja/saiabhiteja.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   * for example your repository is at https://github.com/saiabhiteja/gitprofile, then set base to '/gitprofile/'.
    */
   base: '/gitprofile/',
   projects: {
