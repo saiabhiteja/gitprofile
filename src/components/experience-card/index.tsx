d7c334ae73636b7593b6ddf0bfcd045ac079f22c
saiabhiteja <saiabhiteja29@gmail.com>
2025-07-22 09:08:17 +0530

    updaetd imports

--- src/components/experience-card/index.tsx
+++ src/components/experience-card/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { SanitizedExperience } from '../../interfaces/sanitized-config';
 import { skeleton } from '../../utils';
 import ExperienceModal from '../experience-modal';
